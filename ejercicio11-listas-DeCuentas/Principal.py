3d432412300b69930c61e8505d180fb9e3d9014d
Lunysska <lunysska@gmail.com>
2018-11-21 10:50:15 -0600

    Listas de Cuentas

---
+++ ejercicio11-listas-DeCuentas/Principal.py
@@ -0,0 +1,16 @@
+''' 
+Created on November, 2018 
+@author: lunysska
+
+''' 
+
+from Cuenta import *
+from CuentaAhorro import *
+from CuentaCredito import *
+from MenuUsuario import *
+
+class Principal:
+	pass
+
+	menu = MenuUsuario( )
+	menu.menuCuenta( )
